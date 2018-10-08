dd6eee597112d864cc87904ddf4c426522631ad2
1Mill <erik.ekberg@outlook.com>
2018-10-08 14:36:46 -0700

    Add content to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-	<div>
-		<h1>Hello World!</h1>
+	<div
+	class='[ vh-100 ph4 ba bw1 flex flex-column items-center justify-center ]'
+	>
+		<h1
+		class='[ mt0 f1 lh-title ]'
+		>
+			Erik A. Ekberg
+		</h1>
+
+		<p
+		class='[ lh-copy tl ]'
+		>
+			Full-stack developer who can help you weave bold front-end designs with back-end technical know-how.
+		</p>
 	</div>
 </template>
