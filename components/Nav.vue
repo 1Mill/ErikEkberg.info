0800a866f0ec2f1675a055a084416d03d2ed011a
1Mill <erik.ekberg@outlook.com>
2018-10-08 15:03:51 -0700

    Add navigation menu skeleton

---
+++ components/Nav.vue
@@ -0,0 +1,43 @@
+<template>
+	<div
+	>
+		<div
+		v-if='active'
+		class='[ fixed top-0 left-0 vh-100 w-100 bg-white ]'
+		>
+			OPEN NAV
+		</div>
+
+		<div
+		class='[ fixed bottom-2 right-2 z-1 ]'
+		@click='toggleActive'
+		>
+			<div
+			v-if='active'
+			>
+				CLOSE
+			</div>
+
+			<div
+			v-else
+			>
+				MENU
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data: function () {
+		return {
+			active: true
+		}
+	},
+	methods: {
+		toggleActive: function () {
+			this.active = !this.active
+		}
+	}
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,17 @@
 	<div
 	class='[ tc ]'
 	>
+		<Nav/>
 		<nuxt/>
 	</div>
 </template>
+
+<script>
+import Nav from '~/components/Nav'
+
+export default {
+	components: {
+		Nav
+	}
+}
+</script>
