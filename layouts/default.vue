00c1687acef0dc48796d42b9a91c239c3aa5828a
1Mill <erik.ekberg@outlook.com>
2018-10-26 13:46:56 -0700

    Move layout class to index
    This makes easy page responsible for its own display, and not have to fight against style choices that impact other page rendering.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,7 @@
 	class='[ vh-100 tc flex flex-column items-center justify-center ]'
 	>
 		<Nav/>
-
-		<nuxt
-		class='[ h-100 w-100 ph4 ba bw1 flex flex-column items-center justify-center ][ h-50-ns w-40-ns ][][ w-20-l ]'
-		/>
+		<nuxt/>
 	</div>
 </template>
 
