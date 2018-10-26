442c31fd9eef2134e8c04aa015c1a15cc809e273
1Mill <erik.ekberg@outlook.com>
2018-10-26 12:43:43 -0700

    Add about-me place-holder content

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,5 +1,26 @@
 <template>
-	<div>
-		About me
+	<div
+	class='[ w-100 flex flex-row item-center justify-between ]'
+	>
+		<div
+		class='[ w-50 vh-100 cover ba ]'
+		:style='{ backgroundImage: getImage}'
+		/>
+		<div
+		class='[ w-50 flex flex colu]'
+		>
+			<h1>Erik Ekberg</h1>
+		</div>
 	</div>
 </template>
+
+<script>
+export default {
+	computed: {
+		getImage: function () {
+			const path = require('~/assets/images/ErikEkberg.jpg')
+			return 'url(' + path + ')'
+		}
+	}
+}
+</script>
